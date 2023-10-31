3c0b542e6b376dd62266c060789ce0ad8adf0cf1
amaar <amaar.ahmednur@student.moringaschool.com>
2023-11-01 00:26:12 +0300

    finished assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  const handleQuestionDelete = (questionId) => {
+    // Filtered out the deleted question from the questions array
+    const updatedQuestions = questions.filter((question) => question.id !== questionId);
+    setQuestions(updatedQuestions);
+  };
+
+  const addQuestion = (newQuestion) => {
+    // Sent a POST request to the server to add the new question
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((response) => response.json())
+      .then((createdQuestion) => {
+        // Updated the state with the newly created question
+        setQuestions([...questions, createdQuestion]);
+      });
+  };
+
+  useEffect(() => {
+    // Fetched questions from the server when the component mounts
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data);
+      });
+  }, []); 
 
   return (
     <main>
-      <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      <AdminNavBar onChangePage={handlePageChange} />
+      {page === "Form" ? (
+        <QuestionForm onQuestionSubmit={addQuestion} />
+      ) : (
+        // Rendered the QuestionList component and pass the onDeleteQuestion function
+        <QuestionList questions={questions} onDeleteQuestion={handleQuestionDelete} />
+      )}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionList() {
+function QuestionList({ questions, onDeleteQuestion }) {
+  const handleDeleteClick = (questionId) => {
+    // Sent a DELETE request to your server to delete the question
+    fetch(`http://localhost:4000/questions/${questionId}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.status === 200) {
+        onDeleteQuestion(questionId);
+      }
+    });
+  };
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>
+            <h4>{question.prompt}</h4>
+            <p>Answers:</p>
+            <ul>
+              {question.answers.map((answer, index) => (
+                <li key={index}>{answer}</li>
+              ))}
+            </ul>
+            <button onClick={() => handleDeleteClick(question.id)}>Delete Question</button>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
